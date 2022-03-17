d4da096b1e4282a5f737717e8e321dec01816e9a
Mark Ian Calica <mjc4070@g.rit.edu>
2022-03-16 21:39:56 -0400

    Added in basic movement and basic gameobject

---
+++ game/PredatorBuild/Game1.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace PredatorBuild
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private Predator predator;
+        private Texture2D predTexture;
+        private int width;
+        private int height;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            width = _graphics.GraphicsDevice.Viewport.Width;
+            height = _graphics.GraphicsDevice.Viewport.Height;
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            predator = new Predator(predTexture, new Rectangle(width / 2, height / 2, 40, 40));
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+
+            predator.Draw(_spriteBatch);
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ game/PredatorBuild/Predator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace PredatorBuild
+{
+    class Predator : GameObject
+    {
+        // Parameterized Constructor
+        public Predator(Texture2D texture, Rectangle position) : 
+            base(texture, position)
+        {
+
+        }
+
+        // Movement
+        public override void Update(GameTime gameTime)
+        {
+            KeyboardState keyBState = Keyboard.GetState();
+
+            if (keyBState.IsKeyDown(Keys.Left))
+            {
+                position.X -= 5;
+            }
+            if (keyBState.IsKeyDown(Keys.Right))
+            {
+                position.X += 5;
+            }
+            if (keyBState.IsKeyDown(Keys.Up))
+            {
+                position.Y -= 5;
+            }
+            if (keyBState.IsKeyDown(Keys.Down))
+            {
+                position.Y += 5;
+            }
+        }
+    }
+}
